a1af2eb513852c446752958ff1d82ebd49024683
Bryan Jackson <bjack13@gmail.com>
2017-01-12 17:26:51 -0700

    add in req files

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const session = require('express-session');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const passportGithub2 = require('passport-github2');
+const cors = require('cors');
 
+const config = require('./config.json');
 
 const app = express();
-const port = 3000;
+const port = config.port;
+
+app.use(bodyParser.json());
+app.use(cors());
 
 
-app.listen(port, () =>{
+
+
+app.listen(port, ()=>{
   console.log('it is ALIVE!! on ' + port);
 });
