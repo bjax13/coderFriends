be310a12e87a0020b8b4aadf127c097c89685796
Bryan Jackson <bjack13@gmail.com>
2017-01-12 17:22:38 -0700

    set up routing

--- public/app.js
+++ public/app.js
@@ -1,1 +1,21 @@
-angular.module('app',[]);
+angular.module('app',["ui-router"])
+  .config(function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state('home',{
+        url: "/",
+        templateUrl: "./templates/home"
+      })
+      .state('friends',{
+        url: "/friend/:github_username",
+        templateUrl: "./templates/friends"
+      })
+      .state('login',{
+        url: "/auth/github",
+        templateUrl: "./templates/login"
+      });
+
+
+  });
